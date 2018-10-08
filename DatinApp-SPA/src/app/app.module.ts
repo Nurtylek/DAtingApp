8a202400ade340ddc40cfa3b27933218607aad72
Koshkar Nurtylek <nurtylek98@gmail.com>
2018-10-09 00:36:07 +0600

    Routing Master)))

--- DatinApp-SPA/src/app/app.module.ts
+++ DatinApp-SPA/src/app/app.module.ts
@@ -1,27 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
-
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
-import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
+import { MemberListComponent } from './member-list/member-list.component';
+import { ListsComponent } from './lists/lists.component';
+import { MessagesComponent } from './messages/messages.component';
+
 import { ErrorInterceptorProvider } from './_services/error.interceptor';
+
+import { AuthService } from './_services/auth.service';
 import { AlertifyService } from './_services/alertify.service';
-import { BsDropdownModule } from 'ngx-bootstrap';
+import { AuthGuard } from './_guards/auth.guard';
+
 @NgModule({
    declarations: [
       AppComponent,
       ValueComponent,
       NavComponent,
       HomeComponent,
-      RegisterComponent
+      RegisterComponent,
+      MemberListComponent,
+      ListsComponent,
+      MessagesComponent
    ],
    imports: [
       BrowserModule,
@@ -31,9 +39,10 @@ import { BsDropdownModule } from 'ngx-bootstrap';
       BsDropdownModule.forRoot(),
    ],
    providers: [
-       AuthService,
-       ErrorInterceptorProvider,
-       AlertifyService
+      AuthService,
+      ErrorInterceptorProvider,
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
--- DatinApp-SPA/src/app/nav/nav.component.ts
+++ DatinApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,10 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private http: HttpClient, private alertify: AlertifyService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
   }
@@ -22,6 +26,8 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in successfully');
     }, error => {
       this.alertify.error(error);
+    }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -33,6 +39,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 
